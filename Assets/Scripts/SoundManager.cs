453e868149f57d21dd65ba5327a2dbecf3eaf3e6
prasitex4-source <prasitex4@gmail.com>
2026-03-27 01:01:56 +0100

    meow

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -12,12 +12,18 @@ public enum SoundType
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioClip[] soundList;
+    [SerializeField] private AudioClip[] songsList;
     private static SoundManager instance;
     private AudioSource audioSource;
 
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
     }
 
     public static void PlaySound(SoundType sound, float volume = 1)
